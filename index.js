2381079c80e56776bbfa5c3645c2b80460799853
Dara Hoy <darahoy@gmail.com>
2017-12-05 14:22:04 +0000

    Complete walkthrough

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 /*global $*/
+
+$(document).ready(function() {
+    function getQuote() {
+        $.ajax({
+            method: "GET",
+            url: "https://got-quotes.herokuapp.com/quotes",
+            success: function(response) {
+                $('#quote').text(response.quote);
+                $('#character').text(response.character);
+            }
+        })
+    }
+    getQuote();
+    $('#getQuote').on("click", function() {
+        getQuote();
+    });
+})
